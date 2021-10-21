138f2554d8dcecabb9cbdd381286a7111919c6ef
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-10-21 15:50:24 +0330

    add id to the inpus and ccs class to buttons tag

--- src/components/TransActionForm.js
+++ src/components/TransActionForm.js
@@ -1,54 +1,57 @@
 import { useState } from "react";
 
-const TransActionForm = ({addTransaction}) => {
+const TransActionForm = ({ addTransaction }) => {
   const [formValues, setFormValues] = useState({
     desc: "",
     type: "Expense",
     amount: 0,
-   
-
   });
   const changeHandler = (e) => {
     setFormValues({ ...formValues, [e.target.name]: e.target.value });
   };
-  const submitHandler=(e)=>{
-      e.preventDefault();
-      addTransaction(formValues)
-
-  }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    addTransaction(formValues);
+  };
   return (
     <form onSubmit={submitHandler}>
-      <input
-        type="text"
-        name="desc"
-        onChange={changeHandler}
-        value={formValues.desc}
-      ></input>
-      <input
-        type="number"
-        name="amount"
-        onChange={changeHandler}
-        value={formValues.amount}
-      ></input>
-      <div>
+      {/* <div className="formInput"> */}
+       
+        <input
+          type="text"
+          name="desc"
+          onChange={changeHandler}
+          value={formValues.desc}
+        ></input>
+        <input
+          type="number"
+          name="amount"
+          onChange={changeHandler}
+          value={formValues.amount}
+        ></input>
+      {/* </div> */}
+
+      <div className="radioBox">
         <input
           type="radio"
           value="expense"
           name="type"
           onChange={changeHandler}
           checked={formValues.type === "expense"}
+          id="expense"
         />
-        <label>Expense</label>
+        <label htmlFor="expense">Expense</label>
         <input
           type="radio"
           value="income"
           name="type"
           onChange={changeHandler}
           checked={formValues.type === "income"}
+          id="income"
         />
-        <label>Income</label>
+        <label htmlFor="income">Income</label>
       </div>
-      <button type="submit">Add transaction</button>
+      <button  className="btn primary" type="submit">Add transaction</button>
     </form>
   );
 };
