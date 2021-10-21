9ff495a1c3a322b0a0eb741b8f7c34691e1f0456
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-10-21 15:43:05 +0330

    give the css class to their tags

--- src/components/OverViewComponent.js
+++ src/components/OverViewComponent.js
@@ -1,20 +1,27 @@
 import { useState } from "react";
 import TransActionForm from "./TransActionForm";
 
-const OverViewComponent = ({ income, expense,addTransaction }) => {
+const OverViewComponent = ({ income, expense, addTransaction }) => {
   const [isShow, setIsShow] = useState(false);
   return (
     <>
       <div className="topSection">
         <p>Balance:{income - expense}</p>
-        <button onClick={() => setIsShow((prevState) => !prevState)}>
+        <button
+          className={`btn ${isShow && "cancel"}`}
+          onClick={() => setIsShow((prevState) => !prevState)}
+        >
           {isShow ? "cancel" : "Add"}
         </button>
       </div>
       {isShow && <TransActionForm addTransaction={addTransaction} />}
       <div className="resultSection">
-        <div>Expense {expense}</div>
-        <div>Income {income}</div>
+        <div className="expenseBox">
+          Expense<span style={{ color: "red" }}>{expense}$</span>
+        </div>
+        <div className="expenseBox">
+          Income <span>{income}$</span>
+        </div>
       </div>
     </>
   );
