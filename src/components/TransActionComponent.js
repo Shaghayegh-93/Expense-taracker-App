de25e648c077cd9711a1bc50ba8b6324c38eb7a5
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-10-22 16:28:27 +0330

    added state and functions as a props to this compinent for creating a search box

--- src/components/TransActionComponent.js
+++ src/components/TransActionComponent.js
@@ -1,9 +1,32 @@
-const TransActionComponent = ({ transactions }) => {
+const TransActionComponent = ({
+  transactions,
+  searchItem,
+  changeHandler,
+  filterdTnx,
+}) => {
+  if (!transactions.length) return <h2>add some tranactions</h2>;
   return (
     <section>
-      {transactions.map((t) => (
-        <div key={t.id}>{t.desc} </div>
-      ))}
+      <input
+        type="text"
+        value={searchItem}
+        onChange={changeHandler}
+        placeholder="search for transactions"
+        className="search"
+      />
+
+      {filterdTnx.length
+        ? filterdTnx.map((t) => (
+            <div
+              className="transactions"
+              key={t.id}
+              style={{ borderRight: t.type === "expense" && "4px solid red" }}
+            >
+              <span>{t.desc}</span>
+              <span>{t.amount}</span>
+            </div>
+          ))
+        : "no items matchs!"}
     </section>
   );
 };
